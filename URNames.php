9c936affa0c5511accee62dbdbd0c36fa2bce265
jossmart <josef.martinak@gmail.com>
2012-08-23 13:10:05 +0200

    new file:   URNames.php
    	* This extension gets output html code of
    	some special pages that display user names and joins user real
    	names, if defined.
    
    	* patch set 3
    	* one trailing whitespace removed
    	* spacey style and more - Manual:Coding_conventions
    	* more comments - doxygen style
    	* error in class variables definition corrected
    	* some methods changed from public to private
    
    	* patch set 2
    	* dirname(__FILE__) replaced with __DIR__
    	* trailing whitespaces removed
    	* mixing spaces and tabs for indentation corrected
    
    Change-Id: Ib1e15a416930fe0af6f09fe8f0d0bf8c98f88072

---
+++ URNames.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Extension adds users' real names, if set (only for logged in users) to following pages:
+ * History pages
+ * Recentchanges
+ * Special:Listusers
+ * Special:Activeusers
+ * Special:BlockList
+ */
+
+
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+echo <<<EOT
+	To install my extension, put the following line in LocalSettings.php:
+	require_once( '\$IP/extensions/URNames/URNames.php' );
+EOT;
+exit( 1 );
+}
+
+
+$wgExtensionCredits['specialpage' ][] = array(
+	'name' => 'URNames',
+	'author' => 'Josef Martiňák',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:URNames',
+	'description' => 'Extension displays the real names of wiki users in RecentChanges, PageHistory
+		and users\' lists. Logged in users can see the real name in the brackets after username.',
+	'version' => '0.1'
+);
+
+
+$wgHooks['BeforePageDisplay'][] = 'replaceUserNames';
+
+
+
+/**
+ * Class for replacing output code parts
+ */
+class URNames{
+
+	/**
+	 * @var string $pagename: page name, we want to add a real name to
+	 * @example: 'Listusers', 'Activeusers', 'Blocklist', 'RecentChanges', 'history'
+	 */
+	private $pagename;
+
+	/**
+	 * @var object $user: instance of User
+	 */
+	private $user;
+
+	/**
+	 * @var object $out: instance of OutputPage
+	 */
+	private $out;
+
+
+	/**
+	 * Constructor - init variables
+	 */
+	public function __construct( $pagename, $user, $out ) {
+		$this->pagename = $pagename;
+		$this->user = $user;
+		$this->out = $out;
+	}
+
+
+	/**
+	 * Add real name to output code
+	 */
+	public function replace() {
+		if( $this->pagename == 'Listusers' ) {
+			$pattern = '/title=\"([^\"]*)\">([^<]*)<\/a>/';
+			$callback = preg_replace_callback( $pattern, array( $this, 'listUsersReplace' ),
+				$this->out->mBodytext );
+			return $callback;
+		}
+		else {
+			// default
+			$pattern = '/class=\"mw-userlink\">([^<]*)<\/a>/';
+			$callback = preg_replace_callback( $pattern, array( $this, 'defaultReplace' ),
+				$this->out->mBodytext );
+			return $callback;
+		}
+	}
+
+
+	/**
+	 * Replace code parts - default method
+	 * @param array $matches: found occurances of searched string
+	 * @return code part with real name
+	 */
+	private function defaultReplace( $matches ) {
+		$output = "class=\"mw-userlink\">$matches[1]</a> ";
+		$output .= '(' . $this->user->whoIsReal( $this->user->idFromName( $matches[1] ) ) . ')';
+		$output = preg_replace( '/ \(\)/', '', $output );
+		return $output;
+	}
+
+
+	/**
+	 * Replace code parts in Special:ListUsers
+	 * @param array $matches: found occurances of searched string
+	 * @return code part with real name
+	 */
+	private function listUsersReplace( $matches ) {
+		$output = "title=\"$matches[1]\">$matches[2]</a> ";
+		$output .= '(' . $this->user->whoIsReal( $this->user->idFromName( $matches[2] ) ) . ')';
+		$output = preg_replace( '/ \(\)/', '', $output );
+		return $output;
+	}
+}
+
+
+
+/**
+ * Adds real user names to specific special page or history page
+ * @param object $out: instance of OutputPage
+ * @param object $skin: instance of Skin, unused
+ */
+function replaceUserNames( &$out, &$skin ) {
+	$user = $out->getUser();
+	if( !$user->isLoggedIn() ) {
+		// user is not logged - no action
+		return true;
+	}
+
+	$title = $out->getTitle();
+	$request = $out->getRequest();
+	$query = $request->getQueryValues();
+	$pagename = '';
+
+	if( $title->isSpecialPage() ) {
+		$pagename = SpecialPage::resolveAlias( $title->getBaseText() );
+	}
+	elseif( isset( $query['action'] ) && $query['action'] == 'history' ) {
+		$pagename = 'history';
+	}
+	if( in_array( $pagename, array( 'Recentchanges', 'Activeusers', 'BlockList', 'Listusers', 'history' ) ) ) {
+		$urnames = new URNames( $pagename, $user, $out );
+		$out->mBodytext = $urnames->replace();
+	}
+
+	return true;
+}
+
